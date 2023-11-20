0adb6fe258115de91e8d9295ec74387695d52aa8
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2023-11-20 08:17:37 -0500

    Making an AirBnB Clone

---
+++ AirBnB/gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+function css(done) {
+  // Compilar SAAS
+
+  // Pasos: 1 Identificar Archivo
+  src("src/scss/app.scss")
+    .pipe(sass({ outputStyle: "compressed" })) // 2 - Compilarla
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest("build/css")); // 3 - Guardar el .css
+
+  done();
+}
+
+function images() {
+  return src("src/img/**/*").pipe(dest("build/img"));
+}
+
+function dev() {
+  watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", images);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.images = images;
